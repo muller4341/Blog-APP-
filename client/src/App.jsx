5de9daf4bd631633bf852b5e866b395d9d09dab7
Your Name <mulerwalle@gmail.com>
2024-03-02 21:30:29 +0300

    about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
-
+import  { BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import Signin from './pages/Signin/Signin'
+import Signup from './pages/Signup/Signup'
+import Home from './pages/Home/Home'
+import Dashbord from './pages/Dashbord/Dashbord'
+import Projects from './pages/Projects/Projects'
+import About from './pages/About/About'
 function App() {
   
 
   return (
-    
+<Router>
+<Routes>
 
-    <p className='text-red-400 text-[8px]'> hello muller this is your second full stack project </p>
-      
+<Route path="/" element={<Home/>} />
+<Route path="/signin"element={<Signin/>} />
+<Route path="/signup" element={<Signup/>} />
+<Route path="/dashbord" element={<Dashbord/>} />
+<Route path="/projects" element={<Projects/>} />
+<Route path="/about" element={<About/>} />
+
+
+
+
+  </Routes>
+
+
+
+
+
+
+    </Router>      
   )
 }
 
