1e2396d297ce28f20b7a2d706bfff24d15837733
Your Name <mulerwalle@gmail.com>
2024-03-02 10:55:31 +0300

    Blog App

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+
+  return (
+    
+
+    <p className='text-red-400 text-[8px]'> hello muller this is your second full stack project </p>
+      
+  )
+}
+
+export default App
