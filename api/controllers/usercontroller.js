3cc7b6ded55d3c21c8c7b4999ef0f8acbf23449d
Your Name <mulerwalle@gmail.com>
2024-03-03 21:48:06 +0300

    create a test api

---
+++ api/controllers/usercontroller.js
@@ -0,0 +1,7 @@
+const test= ( req, res)=> {
+    res.json('APi test route is working!')
+
+}
+export { test }
+
+// Compare this snippet from client/src/pages/Projects/Projects.jsx:
--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
-import express from 'express';
-const app = express();
+
 import mongoose from 'mongoose';
+import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import multer from 'multer';
+import userRouter from './routes/userrouter.js'; 
+
+const app = express();
 
 dotenv.config();
-
 
 //mongoose connection
 mongoose.connect(process.env.MONGO_URL)
@@ -14,8 +20,9 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err) => {
     console.log(err);
 });
-
-
+//middleware
+app.use(express.json());
+app.use('/api/user', userRouter);
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
     }
