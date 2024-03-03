3e67b285b284c5b71fed57175fe01b8deb0a96f7
Your Name <mulerwalle@gmail.com>
2024-03-03 15:12:18 +0300

    mongoose connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
 const app = express();
-app.listen(3000, () => {
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+//mongoose connection
+mongoose.connect(process.env.MONGO_URL)
+.then (() => {
+    console.log('Database connected');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+
+app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
     }
 );
+
