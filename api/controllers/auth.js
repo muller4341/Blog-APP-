77c423ad88a76a9e10b9fea5ff66314317b939f1
Your Name <mulerwalle@gmail.com>
2024-03-06 15:27:16 +0300

    add Google Auth functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -78,6 +78,47 @@ const signin =async(req, res, next) => {
 
 };
 
+const google = async (req, res, next) => {
+    const {name, email, googlePhotoUrl} = req.body;
+    
+    try {
+        const user = await User.findOne({email});
 
-export {signup, signin};
+        if (user) {
+            const token = jwt.sign(  {id: user._id}, process.env.JWT_SECRET);
+            const {password, ...rest} = user._doc;    
+            res.status(200).cookie('access_token', token, {
+                httpOnly: true,
+                })
+                .json(rest);
+        }
+        else{
+            const generatedPassword = Math.random().toString(36).slice(-8)
+            + Math.random().toString(36).slice(-8);
+            const hashPassword = bcrypt.hashSync(generatedPassword, 10);
+            const newUser = new User({
+                username: name.toLowerCase().split('').join('') + Math.random().toString(9).slice(-4),
+                email,
+                password: hashPassword,
+                profile: googlePhotoUrl,
+            });
+            const user = await newUser.save();
+            const token = jwt.sign(  {id: user._id}, process.env.JWT_SECRET);
+            const {password, ...rest} = user._doc;
+            res.status(200).cookie('access_token', token, {
+                httpOnly: true,
+                })
+                .json(rest);
+
+        }
+    }
+    catch (error) {
+        next(error);
+    }
+        
+
+}
+
+
+export {signup, signin , google};
 
