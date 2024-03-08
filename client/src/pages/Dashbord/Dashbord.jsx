ee3bc4627c9b8ebe86b5a889a0dfb2c341ae3350
Your Name <mulerwalle@gmail.com>
2024-03-08 15:14:15 +0300

    sidebar of the  dashboard

--- client/src/pages/Dashbord/Dashbord.jsx
+++ client/src/pages/Dashbord/Dashbord.jsx
@@ -1,7 +1,28 @@
+
+import { useLocation, } from "react-router-dom"
+import DashProfile from "./DashProfile";
+import {DashSidebar} from "./DashSidebar";
+import {  useEffect, useState } from "react";
 const Dashbord = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+        if(tabFromUrl){
+          setTab(tabFromUrl);}
+  }, [location.search]);
+
+
   return (
-    <div>
-      <h1>Dashbord</h1>
+    <div className="min-h-screen flex md:flex-row  flex-col ">
+      {/* sidebar*/}
+      <div>
+        <DashSidebar />
+      </div>
+      {/* profile */}
+      {tab==='profile' && <DashProfile />}
+
     </div>
   );
 }
