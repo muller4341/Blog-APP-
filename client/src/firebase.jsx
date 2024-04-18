b5dde0965e82e453507ff4e6c7b01b59f8ac3b24
Muluken Walle <mulerwalle@gmail.com>
2024-04-18 20:35:20 +0300

    add  firebase

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "blog-app1-f5eec.firebaseapp.com",
-  projectId: "blog-app1-f5eec",
-  storageBucket: "blog-app1-f5eec.appspot.com",
-  messagingSenderId: "315232602246",
-  appId: "1:315232602246:web:4d53fcf9b324547f70e907",
-  measurementId: "G-18VC6S6XGJ"
+  apiKey: import.meta.env.VITE_FIREBASE,
+  authDomain: "blog-aeffd.firebaseapp.com",
+  projectId: "blog-aeffd",
+  storageBucket: "blog-aeffd.appspot.com",
+  messagingSenderId: "797959051369",
+  appId: "1:797959051369:web:a4e32bcb6d0d37b752aaf8"
 };
 
 // Initialize Firebase
- export const app = initializeApp(firebaseConfig);  
-  
+export const app = initializeApp(firebaseConfig);
 
+
+  
+  
