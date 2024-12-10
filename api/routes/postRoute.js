9e0218e14b744f235944561b9cc39d5d48dbebca
Muluken Walle <mulerwalle@gmail.com>
2024-12-10 11:42:40 +0300

    create get posts API

--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -2,8 +2,11 @@
 import  express from 'express';
 import verifyUser from '../utils/verifyUser.js'
 import  createPost from '../controllers/createPost.js';
+import getPosts from '../controllers/createPost.js';
 const router= express.Router();
 
 router.post('/create', verifyUser,createPost)
+router.get('/getposts', getPosts)
+
 
 export default router;
