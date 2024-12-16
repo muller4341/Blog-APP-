9cb70dbd7413b26179bd241b17bc7a9019df88f2
Muluken Walle <mulerwalle@gmail.com>
2024-12-16 12:26:19 +0300

    changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import  { BrowserRouter as Router,Route, Routes} from 'react-router-dom'
-import Signin from './pages/SignIn/SignIn'
-import SignUp from './pages/SignUp/SignUp'
-import Home from './pages/Home/Home'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Projects from './pages/Projects/Projects'
-import About from './pages/About/About'
-import Navbar from './pages/Navbar/Navbar'
-import Footer from './pages/footer/footer'
+import Signin from './Pages/SignIn/SignIn'
+import SignUp from './Pages/SignUp/SignUp'
+import Home from './Pages/Home/Home'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import Projects from './Pages/Projects/Projects'
+import About from './Pages/About/About'
+import Navbar from './Pages/Navbar/Navbar'
+import Footer from './Pages/footer/footer'
 import PrivateRoute from './components/PrivateRoute'
-import CreatePost from './pages/createPost/createPost'
+import CreatePost from './Pages/createPost/createPost'
 import AdminPrivateRoute from './components/adminPrivateRoute'
-import UpdatePost from './pages/UpdatePost/UpdatePost'
-
+import UpdatePost from './Pages/UpdatePost/UpdatePost'
+import PostPages from './Pages/PostPages/PostPages.jsx'
+ 
 
 function App() {
   
@@ -20,6 +21,7 @@ function App() {
 <Router>
   <Navbar/>
 <Routes>
+  <Route path="/post/:postSlug" element={<PostPages/>} />
 <Route path="/" element={<Home/>} />
 <Route path="/signin"element={<Signin/>} />
 <Route path="/signup" element={<SignUp/>} />
---
+++ client/src/Pages/PostPages/PostPages.jsx
@@ -0,0 +1,85 @@
+import { set } from "mongoose";
+import { useEffect, useState } from "react"; 
+import { useParams } from "react-router-dom";
+import { Button, Spinner } from 'flowbite-react'
+import { Link } from "react-router-dom";
+
+const PostPages = () => {
+    const {postSlug} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [post, setPost] = useState(null);
+    const [error, setError] = useState(false);
+   
+   
+
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+                const data = await res.json();
+                if (!res.ok) {
+                    console.log(data.message);
+                    return;
+                }
+               if (res.ok) {
+                   setPost(data.posts[0]);
+                   setLoading(false);
+                   setError(false);
+                   return
+               }
+            } catch (error) {
+               
+                setError(true);
+                setLoading(false);
+            }
+        };
+
+        fetchPost();
+    }
+    , [postSlug]);
+    if(loading) 
+        return (
+        <div className="flex justify-center item-center min-h-screen">
+            
+            <Spinner size='xl'/>
+
+
+        </div>
+        );
+
+    return(
+    <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-semibold text-gray-800  my-4">
+            {post && post.title}</h1>
+            <Link to={`/search?category=${post && post.category}`}
+        className='self-center '>
+            <Button color='gray' pill size='xs '>{post && post.category}</Button>
+            </Link>
+            <div className="flex justify-center items-center bg-gray-700">
+            <img src={post && post.image} alt={post && post.title}
+             className="w-2/5 h-1/10 object-cover my-4"/>
+            </div>
+             <div className="flex justify-between p-3 border-b border-slate-500">
+                <span>
+                    {post && new Date(post.createdAt).toLocaleDateString()} 
+                </span>
+
+                <span>{post && (post.content.length/100).toFixed(0)} mins read</span>  
+                
+             </div>
+             <div className="p-3 max-w-2xl mx-auto w-full post-content">
+
+              {post && post.content}
+
+             </div>
+
+    </div>
+    );
+ 
+
+    
+};
+
+export default PostPages;
